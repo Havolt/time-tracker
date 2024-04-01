1be80f404ed9d36a8d2aa7ae5e6bf5a40cd4b50d
Mark <markfitz815@gmail.com>
2024-04-01 21:20:16 +0100

    Add initial times to time tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.scss'
 
 function App() {
+    const [initTime, setInitTime] = useState(null)
+    const [timePassed, setTimePassed] = useState(0)
+    const [startTimer, setStartTimer] = useState(false)
 
-	return (
-		<div id="app">
-			<h1>Time saver</h1>
-		</div>
-	)
+    useEffect(() => {
+        let interval;
+        if(startTimer) {
+            interval = setInterval(() => {
+                setTimePassed(timePassed => timePassed + 1)
+            }, 1000)
+        }
+        return () => clearInterval(interval)
+    }, [startTimer, timePassed])
+
+    const handleTimer = () => {
+        if(!initTime) {
+            setInitTime(new Date())
+            setStartTimer(true)
+        }
+    }
+
+    return (
+        <div id="app">
+            <div>
+                <span>{ timePassed && timePassed }</span>
+                <button onClick={handleTimer}>Go</button>
+            </div>
+        </div>
+    )
 }
 
 export default App
