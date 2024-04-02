d2c72e403ab8931e84fe9334359a45007f01e47e
Mark <markfitz815@gmail.com>
2024-04-02 21:35:31 +0100

    Create a formatted version of time passed

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,42 @@ import { useState, useEffect } from 'react'
 import './App.scss'
 
 function App() {
-    const [initTime, setInitTime] = useState(null)
-    const [timePassed, setTimePassed] = useState(0)
-    const [startTimer, setStartTimer] = useState(false)
+	const [initTime, setInitTime] = useState(null)
+	const [timePassed, setTimePassed] = useState(0)
+	const [startTimer, setStartTimer] = useState(false)
 
-    useEffect(() => {
-        let interval;
-        if(startTimer) {
-            interval = setInterval(() => {
-                setTimePassed(timePassed => timePassed + 1)
-            }, 1000)
-        }
-        return () => clearInterval(interval)
-    }, [startTimer, timePassed])
+	useEffect(() => {
+		let interval;
+		if (startTimer) {
+			interval = setInterval(() => {
+				setTimePassed(timePassed => timePassed + 1)
+			}, 1000)
+		}
+		return () => clearInterval(interval)
+	}, [startTimer, timePassed])
 
-    const handleTimer = () => {
-        if(!initTime) {
-            setInitTime(new Date())
-            setStartTimer(true)
-        }
-    }
+	const handleTimer = () => {
+		if (!initTime) {
+			setInitTime(new Date())
+			setStartTimer(true)
+		}
+	}
 
-    return (
-        <div id="app">
-            <div>
-                <span>{ timePassed && timePassed }</span>
-                <button onClick={handleTimer}>Go</button>
-            </div>
-        </div>
-    )
+	function formatTimePassed() {
+		const date = new Date(0);
+		date.setSeconds(timePassed);
+		const timeString = date.toISOString().substring(11, 19);
+		return timeString;
+	}
+
+	return (
+		<div id="app">
+			<div>
+				<span>{timePassed ? formatTimePassed() : '00:00:00'}</span>
+				<button onClick={handleTimer}>Go</button>
+			</div>
+		</div>
+	)
 }
 
 export default App
