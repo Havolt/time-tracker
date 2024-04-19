6c895bf5f74b7827c10cb758523569ebf858e5f0
Mark <markfitz815@gmail.com>
2024-04-19 18:43:49 +0100

    Add supplemental section to list items

--- src/components/TimeListItem.jsx
+++ src/components/TimeListItem.jsx
@@ -2,9 +2,9 @@ import { useRef, useState } from 'react'
 import { CSSTransition } from 'react-transition-group';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function TimeListItem({key, description, time}) {
+function TimeListItem({ key, description, time }) {
    // TODO: Add an information icon on hover. When clicked the timer expands to show delete and modify buttons.
-   const nodeRef= useRef()
+   const nodeRef = useRef()
 
    const [isHovered, setIsHovered] = useState(false)
 
@@ -12,34 +12,42 @@ function TimeListItem({key, description, time}) {
       console.log('open options');
    }
 
-  return (
+   return (
       <CSSTransition
          in={true}
          nodeRef={nodeRef}
-         timeout={200} 
+         timeout={200}
          classNames="time-list__item"
       >
          <div
             ref={nodeRef}
-            className={`time-list__item ${ isHovered ? 'time-list__item--hover' : ''}`}
+            className={`time-list__item ${isHovered ? 'time-list__item--hover' : ''}`}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
          >
-            <div className="time-list__main">
-               <span>{description}</span>
-               <span>{time}</span>
+            <div className="time-list__full">
+               <div className="time-list__main">
+                  <span>{description}</span>
+                  <div>
+                     <span>{time}</span>
+                     {isHovered &&
+                        <button
+                           onClick={openOptions}
+                           className={'time-list__options'}
+                        >
+                           <FontAwesomeIcon icon={`fa-solid fa-ellipsis`} />
+                        </button>
+                     }
+                  </div>
+               </div>
+               <div className="time-list__supplemental">
+                  test
+               </div>
             </div>
-            { isHovered && 
-               <button
-                  onClick={openOptions}
-                  className={'time-list__options'}
-               >
-                  <FontAwesomeIcon icon={`fa-solid fa-ellipsis`} />
-               </button> 
-            }
+
          </div>
       </CSSTransition>
-  )
+   )
 }
 
 export default TimeListItem
